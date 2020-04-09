d1f8b8cf3edd10f0c141af79ec548581146201a0
Rtvik Gupta <35890433+rtvikgupta@users.noreply.github.com>
2020-04-10 01:47:16 +0530

    Add files via upload

---
+++ lab3/servlet/DbServlet.java
@@ -0,0 +1,45 @@
+package com.capgemini.lab3.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.capgemini.lab3.dbservice.DbConnection;
+
+/**
+ * Servlet implementation class DbServlet
+ */
+public class DbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html><body><table>");
+		out.print("<tr><th>Training Id</th><th>Training Name</th><th>Available Seats</th><th>Action</th></tr>");
+		try {
+			Connection con = DbConnection.getConnection();
+			PreparedStatement ps = con.prepareStatement("select * from training");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				out.print("<tr><td>"+rs.getInt(1)+"</td><td>"+rs.getString(2)+"</td><td>"+rs.getInt(3)+"</td><td><a href='ResponseServlet?tname="+rs.getString(2)+"&&seat="+rs.getInt(3)+"'>Enroll</a></td></tr>");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		out.print("</table></body></html>");
+	}
+
+}
