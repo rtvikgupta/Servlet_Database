39d47450486c95c4f840c6e1611eb6b93282d047
Rtvik Gupta <35890433+rtvikgupta@users.noreply.github.com>
2020-04-10 11:50:06 +0530

    Update DbServlet.java

--- lab3/servlet/DbServlet.java
+++ lab3/servlet/DbServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.capgemini.lab3.dbservice.DbConnection;
+import com.capgemini.lab3.dbservice.DbDaoImpl;
 
 /**
  * Servlet implementation class DbServlet
